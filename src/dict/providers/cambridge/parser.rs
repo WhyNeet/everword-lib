c4c57a14cfcf143b16000c8d5dde6ea3d7cff7ac
ArcSoftwareX <arc.software.x@gmail.com>
2023-12-27 22:07:38 +0300

    feat: merge identical defenitions

--- src/dict/providers/cambridge/parser.rs
+++ src/dict/providers/cambridge/parser.rs
@@ -12,51 +12,77 @@ pub fn parse(doc: &str) -> HashMap<String, Vec<Defenition>> {
     let document = Html::parse_document(doc);
     let defs = document.select(&DEF_ENTRY_SELECTOR);
 
-    defs.into_iter()
-        .fold(HashMap::<String, Vec<Defenition>>::new(), |mut acc, def| {
-            let mut text = def.select(&DEF_BLOCK_SELECTOR);
-            let text = text.next().unwrap().text().collect::<String>();
-            let text = text
-                .trim()
-                .strip_suffix(':')
-                .map(String::from)
-                .unwrap_or(text);
+    /*
+        Get two mappings:
+         - defenition -> part of speech
+         - defenition -> examples
+    */
+    let (def_part_of_speech, mut def_examples) =
+        defs.into_iter()
+            // (HashMap::<defenition, part_of_speech>, HashMap::<defenition, examples>)
+            .fold(
+                (
+                    HashMap::<String, String>::new(),
+                    HashMap::<String, Vec<String>>::new(),
+                ),
+                |(mut parts_of_speech_acc, mut examples_acc), def| {
+                    let mut text = def.select(&DEF_BLOCK_SELECTOR);
+                    let text = text.next().unwrap().text().collect::<String>();
+                    let text = text.trim().strip_suffix(':').unwrap_or(&text).to_string();
 
-            if !text
-                .chars()
-                .all(|c| c.is_alphanumeric() || c.is_whitespace() || c.is_ascii_punctuation())
-            {
-                return acc;
-            }
+                    if !text.chars().all(|c| {
+                        c.is_alphanumeric() || c.is_whitespace() || c.is_ascii_punctuation()
+                    }) {
+                        return (parts_of_speech_acc, examples_acc);
+                    }
 
-            let mut part_of_speech = def.select(&DEF_PART_OF_SPEECH_SELECTOR);
-            let part_of_speech = part_of_speech.next().or_else(|| {
-                let parent = none_return!(def.parent());
-                let header = none_return!(parent.prev_sibling());
-                let header = none_return!(ElementRef::wrap(header));
-                let part_of_speech = header.select(&DEF_PART_OF_SPEECH_SELECTOR).next();
+                    let mut part_of_speech = def.select(&DEF_PART_OF_SPEECH_SELECTOR);
+                    let part_of_speech = part_of_speech.next().or_else(|| {
+                        let parent = none_return!(def.parent());
+                        let header = none_return!(parent.prev_sibling());
+                        let header = none_return!(ElementRef::wrap(header));
+                        let part_of_speech = header.select(&DEF_PART_OF_SPEECH_SELECTOR).next();
 
-                part_of_speech
-            });
+                        part_of_speech
+                    });
 
-            if part_of_speech.is_none() {
-                return acc;
-            }
+                    if part_of_speech.is_none() {
+                        return (parts_of_speech_acc, examples_acc);
+                    }
 
-            let part_of_speech = part_of_speech.unwrap().text().collect::<String>();
+                    let part_of_speech = part_of_speech.unwrap().text().collect::<String>();
 
-            let examples = def.select(&DEF_EXAMPLES_SELECTOR);
-            let examples = examples
-                .into_iter()
-                .map(|example| example.text().collect::<String>().trim().to_string())
-                .collect::<Vec<String>>();
+                    let examples = def.select(&DEF_EXAMPLES_SELECTOR);
+                    let mut examples = examples
+                        .into_iter()
+                        .map(|example| example.text().collect::<String>().trim().to_string())
+                        .collect::<Vec<String>>();
 
-            let defention = Defenition::new(text, examples);
+                    parts_of_speech_acc.insert(text.clone(), part_of_speech);
+                    examples_acc
+                        .entry(text)
+                        .or_insert(Vec::new())
+                        .append(&mut examples);
+
+                    (parts_of_speech_acc, examples_acc)
+                },
+            );
+
+    /*
+        Merge two mappings into: part of speech -> Defenition { ... }
+
+        Reason: defenitions can be duplicated by this provider, so merge their examples.
+    */
+    def_part_of_speech.into_iter().fold(
+        HashMap::<String, Vec<Defenition>>::new(),
+        |mut acc, (def, part_of_speech)| {
+            let examples = def_examples.remove(&def).unwrap();
 
             acc.entry(part_of_speech)
                 .or_insert(Vec::new())
-                .push(defention);
+                .push(Defenition::new(def, examples));
 
             acc
-        })
+        },
+    )
 }
